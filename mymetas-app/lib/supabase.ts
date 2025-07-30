b41fcdb0e1c1ae32b8fba4a6538f1dfdc2a9a4aa
Ian Thalles <ianthalles.in124@gmail.com>
2025-07-30 20:30:17 -0300

    Update supabase.ts

--- mymetas-app/lib/supabase.ts
+++ mymetas-app/lib/supabase.ts
@@ -3,14 +3,19 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 import { Platform } from 'react-native';
 
-const supabaseUrl = 'https://tpswemjkcvrtehuzbfcm.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRwc3dlbWprY3ZydGVodXpiZmNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM3NTQ1MjYsImV4cCI6MjA2OTMzMDUyNn0.AzeuhxAl7xxjWmXUkHIi4ngnSHDgLoPyAHIEw2zlKPI';
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
+
+const authOptions: any = {
+  autoRefreshToken: true,
+  persistSession: true,
+  detectSessionInUrl: false,
+};
+
+if (Platform.OS !== 'web') {
+  authOptions.storage = AsyncStorage;
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  auth: {
-    autoRefreshToken: true,
-    persistSession: true,
-    detectSessionInUrl: false,
-    ...(Platform.OS !== 'web' && { storage: AsyncStorage }),
-  },
+  auth: authOptions,
 });
